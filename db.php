a3c936c726a6acda3e03aeeb06de9ff1b4251529
Willian-Yudy-F <futemawillian.aud@gmail.com>
2026-04-20 13:06:17 +1000

    Update database connection details

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "advanced_web";
+$host = "sql113.infinityfree.com";
+$user = "if0_41704491";
+$pass = "YOUR_PASSWORD_HERE";
+$dbname = "if0_41704491_host";
 $conn = mysqli_connect($host, $user, $pass, $dbname);
-if (!$conn) { die("Erro: " . mysqli_connect_error()); }
+if (!$conn) { die("Connection error: " . mysqli_connect_error()); }
 ?>
